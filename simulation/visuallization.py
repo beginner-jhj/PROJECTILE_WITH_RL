5317f8b3316649875f865c51876ab9ad68e9e31e
beginner-jhj <132197860+beginner-jhj@users.noreply.github.com>
2025-07-11 22:22:38 +0900

    Fix env for constant speed and update visualization

--- simulation/visuallization.py
+++ simulation/visuallization.py
@@ -1,8 +1,8 @@
 from simulation.simul import Simulation
 from simulation.render import Renderer
 
-simulation_result = Simulation(1500, 500).run(
-    angle=50,record_trajectory=True, log_last_position=True)
+simulation_result = Simulation(400, 400).run(
+    angle=50, record_trajectory=True, log_last_position=True)
 
-renderer = Renderer(1100, 500)
+renderer = Renderer(400, 400)
 renderer.render(simulation_result["trajectory"], log=False)
